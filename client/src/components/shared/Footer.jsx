0a30d474d91a8b36ad64a59f53f77610bb708cf8
aquib-11 <135202503+aquib-11@users.noreply.github.com>
2024-12-17 19:31:22 +0530

    kjv

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    <footer className=" text-white py-4 text-center">
+      <p>&copy; 2023 Your Company. All rights reserved.</p>
+      <h1 className="text-2xl font-bold text-yellow-300 hover:text-yellow-100 transition-colors">
+        Hello world!
+      </h1>
+    </footer>
+  );
+};
+export default Footer;
