59212fb8cf7d2ee388d44ebec1f0e5435a64009d
aquib-11 <135202503+aquib-11@users.noreply.github.com>
2025-01-11 11:18:31 +0530

    dcasc

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,12 +4,13 @@ const router = Router();
 
 import { login, logout, register } from "../controllers/authController.js";
 import { validateLoginInput, validateRegisterInput } from "../middleware/validationMiddleware.js";
+import { getUserRole } from "../controllers/userController.js";
 
 router.post("/register",validateRegisterInput, register);
 router.post("/login",validateLoginInput, login);
 router.get("/logout",  logout);
 // router.post("/verify-email", verifyEmail);
-// router.get("/userRole",getUserRole )
+router.get("/userRole",getUserRole )
 // router.get("/admin-data", getAdminData);
 // router.patch(
 //   "/update-user-details/:id",
